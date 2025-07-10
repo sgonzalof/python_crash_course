70d0e9ee0614dd44cd59ee00ace6e26022dccdb8
sgonzalof <sgonz@HOMEPC>
2025-07-11 00:35:24 +0200

    free_prox

--- clase_python_01/06_scraping/04_proxy_scrape.py
+++ clase_python_01/06_scraping/04_proxy_scrape.py
@@ -1,11 +1,17 @@
 import requests
+import time
+from pprint import pprint
+from bs4 import BeautifulSoup
 
 
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"
+    }
 def list_proxies():
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"}
+
     url = "https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&proxy_format=protocolipport&format=text"
     try:
-        response = requests.get(url, headers=headers, timeout=10)
+        response = requests.get(url, headers=HEADERS, timeout=10)
         response.raise_for_status()
         proxies = response.text.splitlines()
         return proxies
@@ -15,22 +21,30 @@ def list_proxies():
     
 
 
-def extract_elite_proxies():
-    elite_proxies = []
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"}
-    url = "https://api.proxyscrape.com/v4/free-proxy-list/get?request=get_proxies&skip=0&proxy_format=protocolipport&format=json&limit=15"
-    response = requests.get(url, headers=headers, timeout=10)
-    datos = response.json()
-    try:
-        response = requests.get(url, headers=headers, timeout=10)
-        response.raise_for_status()
-        datos = response.json()
-        for key, value in datos["proxies"].items():
-            if value["annonymity"] == "elite":
-                elite_proxies.append(f"{value['protocol']}://{value['ip']}:{value['port']}")
-    except requests.RequestException as e:
-        print(f"Error fetching elite proxy list: {e}")
-    return elite_proxies
+def free_proxie_list():
+    r = requests.get("https://free-proxy-list.net/", headers=HEADERS, timeout=10)
+
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    lines = soup.find("tbody").find_all("tr")
+    proxies ={}
+    for line in lines:
+        values = line.find_all("td")
+        proxy_ip = values[0].text
+        proxy_port = values[1].text
+        proxy_type = values[4].text
+
+        if proxy_type == "elite_proxy":
+            proxy = f'{proxy_ip}:{proxy_port}'
+            proxies.append(proxy)
+        print(proxies)
+
+    # return {
+    #     "origen": "Free Proxy List:",
+    #     "lista": proxies
+    # }
+    return print(proxies)
 
 
-print(extract_elite_proxies())
+
+free_proxie_list()
